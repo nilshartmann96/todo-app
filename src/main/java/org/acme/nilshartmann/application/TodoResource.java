48ce4e4a74ac65bd48d780effb94791bb66e587d
nilshartmann96 <hartmann_nils@outlook.de>
2021-08-16 10:00:38 +0200

    ELAB-475 Initialer Commit mit richtiger Ordnerstruktur und Benennung von Test-Files

--- src/main/java/org/acme/nilshartmann/application/TodoResource.java
+++ src/main/java/org/acme/nilshartmann/application/TodoResource.java
@@ -1,12 +1,12 @@
-package org.acme;
+package org.acme.nilshartmann.application;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/todo")
+public class TodoResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
